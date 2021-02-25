2ca297c1bc0eebfb645f1cf4543d3b5f03d5a2a0
Manuel Burger <burgerm@student.ethz.ch>
2021-02-25 19:58:09 +0100

    Update constructors

--- src/data/objects.py
+++ src/data/objects.py
@@ -6,16 +6,27 @@
 # ------------------------------
 class Street():
 
-    def __init__(self) -> None:
+    def __init__(self, name, start_section, end_section, L) -> None:
         
-        pass
-
+        self.name = name
+        self.L = L
+        self.start_section = start_section
+        self.end_section = end_section
 
 
 class Intersection():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, id) -> None:
+        
+        self.id = id
+
+        self.incoming_sections = []
+        self.outgoing_sections = []
+
+        self.outgoing_streets = []
+        self.incoming_streets = []
+
+
 
 
 
