5ccab756ca09a6ef3c81f4b576cf69cd7681119e
Manuel Burger <burgerm@student.ethz.ch>
2021-02-25 19:54:31 +0100

    Add classes

---
+++ src/data/objects.py
@@ -0,0 +1,25 @@
+
+# -----------------------------
+#
+# Street Class
+#
+# ------------------------------
+class Street():
+
+    def __init__(self) -> None:
+        
+        pass
+
+
+
+class Intersection():
+
+    def __init__(self) -> None:
+        pass
+
+
+
+class Car():
+
+    def __init__(self) -> None:
+        pass
