f8f9611a5623b53244be9325ebf90ef83dee04c2
Manuel Burger <burgerm@student.ethz.ch>
2021-02-25 20:52:22 +0100

    Output write

--- src/data/output.py
+++ src/data/output.py
@@ -14,6 +14,10 @@ class Output():
         self.file = open(filepath, "w")
         self.name = name
 
+        # Class fields
+        self.no_intersections = 0
+        self.intersections = []
+
     def write(self):
         """
         Parse Input File and Store in local data structure
@@ -21,8 +25,24 @@ class Output():
             - input: Object of class Input
             - parameters: dictionary of parameters for algorithm
         """
-        # raise NotImplementedError("Implement Output write method")
-        
-        self.file.write("test")
+
+        # No of intersections for which we set a schedule
+        self.file.write("{}\n".format(self.no_intersections))
+
+        # Output each schedule
+        for inter in self.intersections:
+
+            schedule = []
+            for street in inter.schedule.order:
+
+                d = inter.schedule.durations[street]
+                if d > 0:
+                    schedule.append((street.name, d))
+
+            if len(schedule) > 0:
+                self.file.write("{}\n".format(inter.id))
+                self.file.write("{}\n".format(len(schedule)))
+                for s in schedule:
+                    self.file.write("{} {}".format(s[0], s[1]))
 
         self.file.close()
