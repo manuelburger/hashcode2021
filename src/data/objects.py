ff5f7d7015b9b10301d14be3b747522a39455087
Manuel Burger <burgerm@student.ethz.ch>
2021-02-25 20:33:20 +0100

    String stuff

--- src/data/objects.py
+++ src/data/objects.py
@@ -13,6 +13,9 @@ class Street():
         self.start_section = start_section
         self.end_section = end_section
 
+    def __str__(self) -> str:
+        return "Street: {}".format(self.name)
+
 
 class Intersection():
 
@@ -31,6 +34,8 @@ class Intersection():
     def set_schedule(schedule):
         self.schedule = schedule
 
+    def __str__(self) -> str:
+        return "Inter: {}".format(self.id)
 
 
 class Schedule():
@@ -44,13 +49,48 @@ class Schedule():
         for street in streets:
             self.durations[street] = 0
 
+    def set_street_to_front(street):
+
+        if street in self.order:
+
+            new_order = [street]
+            for s in self.order:
+                if not street == s:
+                    new_order.append(s)
+
+            if len(new_order) == len(self.order):
+                self.order = new_order
+            else:
+                print("WARNING: new order different length from old order in schedule")
+
+        else:
+            print("WARNING: tried to set non-existent street on schedule")
+
+
+    def set_duration_street(street, duration):
+        self.durations[street] = duration
+
+
+    def __str__(self) -> str:
+        
+        out = ""
+        for s in self.order:
+            out = out + (s, self.durations[s])
+
+
 
 
 class Car():
 
-    def __init__(self) -> None:
+    def __init__(self, id) -> None:
+
+        self.id = id
         
         self.total_streets = 0
 
         self.street_path = []
         self.intersection_path = []
+
+
+    def __str__(self) -> str:
+        return "Car: {}".format(self.id)
